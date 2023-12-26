108543bcd87346b30bddc77e61c69641d227226b
kim-seonwoo <sw204011@gmail.com>
2023-12-26 14:38:24 +0900

    [Set] init commit

---
+++ Netflix-iOS/Netflix-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix-iOS
+//
+//  Created by Seonwoo Kim on 12/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
